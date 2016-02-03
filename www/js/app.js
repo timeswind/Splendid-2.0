86b5ffde935ac19ce562198c068849cff204da28
timeswind <timeswind@Timeswinds-MacBook-Pro.local>
2016-02-03 18:06:19 +0800

    UPDATE AND FIX BUGS

--- www/js/app.js
+++ www/js/app.js
@@ -1,141 +1,73 @@
-/* global StatusBar */
-/* global cordova */
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ionic.service.core', 'ionic.service.push', 'ngCordova', 'ionic.service.analytics', 'starter.controllers', 'starter.services', 'ngIOS9UIWebViewPatch'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
-  .run(function ($ionicPlatform, $ionicAnalytics, $cordovaSplashscreen) {
-    $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      setTimeout(function () {
-        $cordovaSplashscreen.hide()
-      }, 3000)
-      $ionicAnalytics.register();
-      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleLightContent();
-      }
-    });
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
   })
 
-  .config(function ($stateProvider, $urlRouterProvider) {
-
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
-
-    // setup an abstract state for the tabs directive
-      .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html',
-      })
-
-    // Each tab has its own nav history stack:
-
-      .state('tab.dash', {
-        url: '/dash',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/tab-dash.html',
-            controller: 'DashCtrl'
-          }
-        }
-      })
-      .state('tab.fullscheduleview', {
-        url: '/dash/fullscheduleview',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/fullscheduleview.html',
-            controller: 'FullScheduleCtrl'
-          }
-        }
-
-      })
-
-      .state('tab.account', {
-        url: '/account',
-        views: {
-          'tab-account': {
-            templateUrl: 'templates/tab-account.html',
-            controller: 'AccountCtrl'
-          }
-        }
-      })
-      .state('tab.calendar', {
-        url: '/calendar',
-        views: {
-          'tab-calendar': {
-            templateUrl: 'templates/tab-calendar.html',
-            controller: 'CalendarCtrl'
-          }
-        }
-      })
-      .state('tab.allcalendarview', {
-        url: '/calendar/allcalendarview/:EventType',
-        views: {
-          'tab-calendar': {
-            templateUrl: 'templates/allcalendarview.html',
-            controller: 'AllCalendarCtrl'
-          }
-        }
-      });
-
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/dash');
-
-  });
-
-angular.module('ngIOS9UIWebViewPatch', ['ng']).config(['$provide', function ($provide) {
-  'use strict';
-
-  $provide.decorator('$browser', ['$delegate', '$window', function ($delegate, $window) {
-
-    if (isIOS9UIWebView($window.navigator.userAgent)) {
-      return applyIOS9Shim($delegate);
-    }
-
-    return $delegate;
-
-    function isIOS9UIWebView(userAgent) {
-      return /(iPhone|iPad|iPod).* OS 9_\d/.test(userAgent) && !/Version\/9\./.test(userAgent);
-    }
-
-    function applyIOS9Shim(browser) {
-      var pendingLocationUrl = null;
-      var originalUrlFn = browser.url;
-
-      browser.url = function () {
-        if (arguments.length) {
-          pendingLocationUrl = arguments[0];
-          return originalUrlFn.apply(browser, arguments);
-        }
-
-        return pendingLocationUrl || originalUrlFn.apply(browser, arguments);
-      };
-
-      window.addEventListener('popstate', clearPendingLocationUrl, false);
-      window.addEventListener('hashchange', clearPendingLocationUrl, false);
-
-      function clearPendingLocationUrl() {
-        pendingLocationUrl = null;
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
       }
-
-      return browser;
     }
-  }]);
-}]);
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,460 +1,56 @@
-/* global angular */
 angular.module('starter.controllers', [])
 
-  .controller('DashCtrl', function ($scope, $filter, $http, $state, GetSchedule, TodayData, DateDifference) {
-    var weekdayarray = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"];
-    var weekdayinthreedays = [];
-    var montharray = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
-
-    var schoolday = "";
-    var schooldayfortomorrow = "";
-    var holiday = "";
-    var holidayfortomorrow = "";
-    var activity = "";
-    var activityfortomorrow = "";
-
-    var thegrade;
-    var theclass;
-
-    $scope.schooldayboolean = false;
-    $scope.schooldayfortomorrowboolean = false;
-
-    $scope.schoolactivityboolean = false;
-    $scope.schoolactivityfortomorrowboolean = false;
-
-    $scope.displayinstruction = false;
-
-    //进入View
-    $scope.$on('$ionicView.enter', function () {
-      var today = new Date();
-      weekdayinthreedays = [];
-
-      var weekday = weekdayarray[today.getDay()];
-      var month = montharray[today.getMonth()];
-
-      //DSE倒计时计算
-      if (window.localStorage['dse']) {
-        if (window.localStorage['dse'] == "true") {
-          var dseDate = new Date("03/30/2016");
-          $scope.dseboolean = true
-          $scope.dse = DateDifference.daysBetween(today, dseDate)
-        } else {
-          $scope.dseboolean = false
-        }
-      } else {
-        $scope.dseboolean = false;
-      }
-      
-      //检查课程表设置
-      checkScheduleSettings()
-      
-      //标题显示今天 
-      $scope.today = weekday + " " + month + today.getDate() + "日"
-
-      //未来两天
-
-      weekdayinthreedays.push(weekday);
-      weekdayinthreedays.push(weekdayarray[today.getDay() + 1]);
-
-      $scope.weekdayinthreedays = weekdayinthreedays
-
-      
-      //天气
-      if (window.localStorage['weather']) {
-        if (window.localStorage['weather'] == "true") {
-          $http({
-            method: 'GET',
-            url: 'https://api.heweather.com/x3/weather?cityid=CN101320101&key=3b73af05dcba475c9318e6adb024a480',
-          }).success(function (data) {
-
-            $scope.weatherdescription = $scope.temperature = data["HeWeather data service 3.0"][0]["now"]["cond"]["txt"];
-            console.log(data["HeWeather data service 3.0"][0])
-            $scope.temperature = data["HeWeather data service 3.0"][0]["now"]["tmp"];
-            $scope.maxtemperature = data["HeWeather data service 3.0"][0]["daily_forecast"][0]["tmp"]["max"];
-            $scope.mintemperature = data["HeWeather data service 3.0"][0]["daily_forecast"][0]["tmp"]["min"];
-                        $scope.weathersuggestion = data["HeWeather data service 3.0"][0]["suggestion"]["comf"]["txt"];
-            // $scope.humidity = data.main.humidity;
-
-          }).error(function (data) {
-            console.log("!!Get Weather Data Failed!!")
-          });
-          $scope.weatherboolean = true
-
-        } else {
-          $scope.weatherboolean = false
-        }
-      } else {
-        $scope.weatherboolean = false;
-      }
-
-
-      
-      //显示活动，假期，课程表
-      setCalender();
-      $scope.$broadcast('scroll.refreshComplete');
-
-
-    });
-    //下拉刷新
-    $scope.doRefresh = function () {
-      $scope.$broadcast('$ionicView.enter');
-    }
-
-    //检查课程表设置
-    function checkScheduleSettings() {
-      if (window.localStorage['grade']) {
-        thegrade = window.localStorage['grade'];
-        $scope.grade = thegrade;
-        $scope.displayinstruction = false;
-      } else {
-        thegrade = null;
-        $scope.displayinstruction = true;
-
-      }
-
-      if (window.localStorage['class']) {
-        theclass = window.localStorage['class'];
-        $scope.class = theclass;
-        $scope.displayinstruction = false;
-      } else {
-        theclass = null;
-        $scope.displayinstruction = true;
-      }
-    }
-
-    //當在設置里改變了課程表后
-    $scope.$on('changeSchedule', function (e, msg) {
-
-      if (window.localStorage['grade']) {
-        thegrade = window.localStorage['grade'];
-        $scope.grade = thegrade;
-        if (window.localStorage['class']) {
-          theclass = window.localStorage['class'];
-          $scope.class = theclass;
-          if ($scope.schooldayboolean) {
-            getSchedule(schoolday)
-            $scope.displayinstruction = false;
-          }
-        } else {
-          theclass = null;
-          $scope.displayinstruction = true;
-        }
-      } else {
-        thegrade = null;
-        $scope.displayinstruction = true;
-      }
-
-    });
-    //獲取活動，假期以及課程表
-    function setCalender() {
-
-      var today = new Date();
-      var tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-      var daytest = $filter('date')(today, 'dd/MM/yyyy');
-      var tomorrowtest = $filter('date')(tomorrow, 'dd/MM/yyyy');
-
-      $http.get('calender.json').then(function (data) {
-
-        schoolday = eval(data.data[daytest])["day"];
-        TodayData.update(schoolday);
-
-        schooldayfortomorrow = eval(data.data[tomorrowtest])["day"];
-
-        holiday = eval(data.data[daytest])["holiday"];
-        holidayfortomorrow = eval(data.data[tomorrowtest])["holiday"];
-
-        activity = eval(data.data[daytest])["activity"];
-        activityfortomorrow = eval(data.data[tomorrowtest])["activity"];
-
-        if (schoolday !== "") {
-          $scope.schooldayboolean = true;
-          $scope.schoolday = "Day" + " " + schoolday;
-          getSchedule(schoolday);
-        } else if (holiday !== "") {
-          $scope.schooldayboolean = false;
-          $scope.holiday = holiday;
-        } else {
-          $scope.schooldayboolean = false;
-        }
-
-        if (activity !== "") {
-          $scope.schoolactivityboolean = true;
-          $scope.activity = activity;
-        } else {
-          $scope.schoolactivityboolean = false;
-        }
-
-        if (schooldayfortomorrow !== "") {
-          $scope.schooldayfortomorrowboolean = true;
-          $scope.schooldayfortomorrow = "Day" + " " + schooldayfortomorrow;
-        } else if (holidayfortomorrow !== "") {
-          $scope.schooldayfortomorrowboolean = false;
-          $scope.holidayfortomorrow = holidayfortomorrow;
-          $scope.schooldayfortomorrow = "假期"
-        } else {
-          $scope.schooldayfortomorrowboolean = false;
-          $scope.schooldayfortomorrow = "無"
-        }
-
-        if (activityfortomorrow !== "") {
-          $scope.schoolactivityfortomorrowboolean = true;
-          $scope.activityfortomorrow = activityfortomorrow;
-        } else {
-          $scope.schoolactivityfortomorrowboolean = false;
-        }
-
-      });
-    }
-    //根據日子獲取課程表
-    function getSchedule(day) {
-      if ($scope.grade && $scope.class) {
-        GetSchedule.all().then(function (data) {
-          $scope.todayScheduleData = data.data[thegrade + theclass][0][day];
-        });
-      } else {
-        $scope.displayinstruction = true;
-        console.log("class or grade not set")
-      }
-
-    }
-
-  })
-
-
-  .controller('AccountCtrl', function ($scope, $rootScope) {
-
-    $scope.settings = {
-      dse: null,
-      weather: null
-    }
-
-    if (window.localStorage['dse']) {
-      if (window.localStorage['dse'] == "true") {
-        $scope.settings.dse = true;
-      } else {
-        $scope.settings.dse = false;
-      }
-    } else {
-      window.localStorage['dse'] = "true"
-      $scope.settings.dse = true;
-    }
-
-    if (window.localStorage['weather']) {
-      if (window.localStorage['weather'] == "true") {
-        $scope.settings.weather = true;
-      } else {
-        $scope.settings.weather = false;
-      }
-    } else {
-      window.localStorage['weather'] = "true"
-      $scope.settings.weather = true;
-    }
-
-
-
-
-    $scope.set = function (name) {
-
-      var settingDictionary = $scope.settings;
-      var settingArray = Object.keys(settingDictionary)
-      console.log(settingArray.indexOf(name))
-      var index = settingArray.indexOf(name)
-
-      if (window.localStorage[name]) {
-        if (window.localStorage[name] == "true") {
-          window.localStorage[name] = "false";
-          $scope.settings[index] = false;
-        } else if (window.localStorage[name] == 'false') {
-          window.localStorage[name] = 'true';
-          $scope.settings[index] = true;
-        }
-      } else {
-        window.localStorage[name] = 'true';
-        $scope.settings[index] = true;
-      }
-    }
-
-
-
-    $scope.data = {
-      classSelect: window.localStorage['class'],
-      gradeSelect: window.localStorage['grade'],
-      classOptions: [
-        { id: 'A' },
-        { id: 'B' },
-        { id: 'C' },
-        { id: 'D' },
-      ],
-      gradeOptions: [
-        { id: '1', name: '中一' },
-        { id: '2', name: '中二' },
-        { id: '3', name: '中三' },
-        { id: '4', name: '中四' },
-        { id: '5', name: '中五' },
-        { id: '6', name: '中六' }
-      ]
-    };
-
-    $scope.updateGrade = function (selectgrade) {
-      window.localStorage.setItem('grade', selectgrade);
-      $rootScope.$broadcast('changeSchedule', 'hi');
-    }
-
-    $scope.updateClass = function (selectclass) {
-      window.localStorage.setItem('class', selectclass);
-      $rootScope.$broadcast('changeSchedule', 'hi');
-    }
-  })
-
-  .controller('FullScheduleCtrl', function ($scope, $rootScope, GetSchedule, TodayData) {
-    var thegrade;
-    var theclass;
-    var schoolday = TodayData.getSchoolday();
-    var chineseGrade = ["零", "一", "二", "三", "四", "五", "六"]
-
-    $scope.schoolday = schoolday;
-
-    GetSchedule.all().then(function (data) {
-      $scope.todayScheduleData = data.data[thegrade + theclass][0][schoolday];
-    });
-
-    $scope.displayinstruction = false;
-
-    $scope.data = {
-      dayselect: schoolday
-    }
-
-    $scope.dayselect = [
-      { value: "A" },
-      { value: "B" },
-      { value: "C" },
-      { value: "D" },
-      { value: "E" },
-      { value: "F" }
-    ];
-
-    if (window.localStorage['grade']) {
-      thegrade = window.localStorage['grade'];
-      $scope.grade = chineseGrade[thegrade];
-      if (window.localStorage['class']) {
-        theclass = window.localStorage['class'];
-        $scope.class = theclass;
-      } else {
-        theclass = null;
-        $scope.displayinstruction = true;
-      }
-    } else {
-      thegrade = null;
-      $scope.displayinstruction = true;
-    }
-
-    $scope.getSchedule = function (day) {
-      getSchedule(day)
-    }
-
-    $scope.$on('changeSchedule', function (e, msg) {
-
-      if (window.localStorage['grade']) {
-        thegrade = window.localStorage['grade'];
-        $scope.grade = thegrade;
-        $scope.grade = chineseGrade[thegrade];
-        if (window.localStorage['class']) {
-          theclass = window.localStorage['class'];
-          $scope.class = theclass;
-          getSchedule($scope.data.dayselect)
-          if ($scope.schooldayboolean) {
-            $scope.displayinstruction = false;
-          }
-        } else {
-          theclass = null;
-          $scope.displayinstruction = true;
-        }
-      } else {
-        thegrade = null;
-        $scope.displayinstruction = true;
-      }
-
-    });
-
-    function getSchedule(day) {
-      GetSchedule.all().then(function (data) {
-        $scope.todayScheduleData = data.data[thegrade + theclass][0][day];
-      });
-    }
-
-  })
-
-  .controller('CalendarCtrl', function ($scope, $http, $filter, DateDifference) {
-
-    var holidayArray = [];
-    var activityArray = [];
-    var today = new Date();
-    var start = new Date();
-    var end = new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000);
-
-    $http.get('calender.json').then(function (data) {
-
-      while (start < end) {
-        var holidayName = eval(data.data[$filter('date')(start, 'dd/MM/yyyy')])["holiday"];
-        var activityName = eval(data.data[$filter('date')(start, 'dd/MM/yyyy')])["activity"];
-        var dateFormate = $filter('date')(start, 'MM月dd日')
-        if (holidayName) {
-          holidayArray.push({ date: dateFormate, name: holidayName, countdown: DateDifference.daysBetween(today, start) });
-
-        }
-
-        if (activityName) {
-          activityArray.push({ date: dateFormate, name: activityName, countdown: DateDifference.daysBetween(today, start) });
-
-        }
-        var newDate = start.setDate(start.getDate() + 1);
-        start = new Date(newDate);
-      }
-
-      $scope.holiday = holidayArray
-      $scope.activity = activityArray
-
-    });
-  })
-  .controller('AllCalendarCtrl', function ($scope, $stateParams, $http, $filter, DateDifference) {
-    $scope.holidayCalendar = false;
-    if ($stateParams.EventType) {
-      if ($stateParams.EventType == "holiday") {
-        $scope.title = "所有假期";
-        $scope.holidayCalendar = true;
-        pushEvents("holiday");
-      } else if ($stateParams.EventType == "activity") {
-        $scope.title = "所有活動和事項";
-        $scope.holidayCalendar = false;
-        pushEvents("activity");
-      }
-    }
-
-    var eventArray = [];
-
-    var today = new Date();
-    var start = new Date();
-    var end = new Date("07/31/2016");
-
-    function pushEvents(EventType) {
-      $http.get('calender.json').then(function (data) {
-
-        while (start < end) {
-          var eventName = eval(data.data[$filter('date')(start, 'dd/MM/yyyy')])[EventType];
-
-          var dateFormate = $filter('date')(start, 'MM月dd日')
-          if (eventName) {
-            eventArray.push({ date: dateFormate, name: eventName, countdown: DateDifference.daysBetween(today, start) });
-
-          }
-          var newDate = start.setDate(start.getDate() + 1);
-          start = new Date(newDate);
-        }
-
-        $scope.events = eventArray
-
-      });
-    }
-
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
   });
-  
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
